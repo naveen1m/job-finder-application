323012a48b541673d93aee4efb2f818e195d758f
naveen <stupiddint@gmail.com>
2024-12-23 10:02:17 +0530

    update: ui

--- job-finder-frontend/src/components/Navbar.jsx
+++ job-finder-frontend/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   const isSeeker = role === "seeker";
 
   return (
-    <div className="navbar bg-white px-4">
+    <div className="navbar fixed bg-white px-4 shadow-lg rounded-lg">
       <div className="flex items-center space-x-2">
         <img src="/icon.png" alt="logo" className="h-8 w-8" />
         <h1
@@ -23,60 +23,62 @@ const Navbar = () => {
         </h1>
       </div>
 
-      <div className="flex items-center space-x-4 ml-auto">
-        <div className="flex justify-end">
-          <button
-            onClick={() => {
-              navigate(isSeeker ? "/jobs" : "/jobs/post");
-            }}
-            className="bg-blue-600 text-white px-4 py-2 rounded-md"
-          >
-            {isSeeker ? "View Jobs" : "Post Job"}{" "}
-          </button>
-        </div>
-        {/* Profile Icon */}
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              />
-            </div>
+      {token && (
+        <div className="flex items-center space-x-4 ml-auto">
+          <div className="flex justify-end">
+            <button
+              onClick={() => {
+                navigate(isSeeker ? "/jobs" : "/jobs/post");
+              }}
+              className="bg-blue-600 text-white px-4 py-2 rounded-md"
+            >
+              {isSeeker ? "View Jobs" : "Post Job"}{" "}
+            </button>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-white rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a
-                className="justify-between text-black"
-                onClick={() => {
-                  navigate("/profile");
-                }}
-              >
-                Profile
-              </a>
-            </li>
-            {token && (
-              <li className="text-black">
+          {/* Profile Icon */}
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-white rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              <li>
                 <a
+                  className="justify-between text-black"
                   onClick={() => {
-                    localStorage.clear();
-                    navigate("/");
+                    navigate("/profile");
                   }}
                 >
-                  Logout
+                  Profile
                 </a>
               </li>
-            )}
-          </ul>
+              {token && (
+                <li className="text-black">
+                  <a
+                    onClick={() => {
+                      localStorage.clear();
+                      navigate("/");
+                    }}
+                  >
+                    Logout
+                  </a>
+                </li>
+              )}
+            </ul>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
